983aa87febed36ac1c40ac945a9a4a933c181792
Alexey Kosinov <kosinov@protonmail.com>
2021-09-07 12:56:50 +0300

    impact vs promgen

--- sdk/bootloader.bat
+++ sdk/bootloader.bat
@@ -1,7 +1,8 @@
 @echo off
 elfcheck -hw ml605/system.xml -mode bootload -mem BRAM -pe microblaze_0 bootloader/Debug/bootloader.elf 
 data2mem -bm ml605/system_bd.bmm -bt ml605/system.bit -bd bootloader/Debug/bootloader.elf tag microblaze_0 -o b ml605/bootloader.bit
-promgen -w -p mcs -c FF -o ml605/bootloader -x xcf128x -u 0 ml605/bootloader.bit -bpi_dc parallel -data_width 16  
+rem promgen -w -p mcs -c FF -o ml605/bootloader -x xcf128x -u 0 ml605/bootloader.bit -bpi_dc parallel -data_width 16  
 impact -batch ../system.impact
 rm *.log
+rm ml605/*_cclktemp.bit
 pause
