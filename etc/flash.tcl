7c1a1613ec912afe9d93338d6faedd22de3603b7
Alexey Kosinov <kosinov@protonmail.com>
2021-09-09 11:59:20 +0300

    correct ddr offset

--- etc/flash.tcl
+++ etc/flash.tcl
@@ -3,7 +3,7 @@ set FLASH_FILE              "main/Debug/main.srec"
 set FLASH_BASEADDR          0x42000000 
 set FLASH_PROG_OFFSET       0x00F00000
 set FLASH_BUSWIDTH          16
-set SCRATCH_BASEADDR        0x60000000
+set SCRATCH_BASEADDR        0xC0000000
 set SCRATCH_LEN             0x10000000
 set TARGET_TYPE             "MICROBLAZE"
 set PROC_INSTANCE           "microblaze_0"
